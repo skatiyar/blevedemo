1abd8f51a93ae169f51bfd0acf7bf89930a61fe6
Suyash Katiyar <katiyar.suyash@gmail.com>
2017-12-16 04:01:22 +0530

    added result display

--- public/script.js
+++ public/script.js
@@ -1,12 +1,27 @@
 (function($) {
   var searchBtn = $('#search-button');
+  var results = $('.Search__Results');
 
   searchBtn.on('click', function(e) {
     var query = encodeURIComponent($('#search-input').val());
 
     $.get('/search?q=' + query)
     .then(function(res) {
-      console.log(res);
+      var data = JSON.parse(res);
+      var resultsHtml = '';
+      for (var i = 0; i < data.length; i++) {
+        resultsHtml += '<div class="Result"><div class="Id">' + data[i].id + '</div>';
+        var frags = Object.keys(data[i].fragments);
+        for (var j = 0; j < frags.length; j++) {
+          resultsHtml += '<div class="Field">' + frags[j] + '</div>';
+          for (var x = 0; x < data[i].fragments[frags[j]].length; x++) {
+            resultsHtml += '<div class="Frag">' + data[i].fragments[frags[j]][x] + '</div>';
+          }
+        }
+        resultsHtml += '</div>';
+      }
+      results.empty();
+      results.append(resultsHtml);
     })
     .catch(function(err) {
       alert(JSON.stringify(err));
