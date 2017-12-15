673545a3cabbe82e24f67d40166a3767792f7446
Suyash Katiyar <katiyar.suyash@gmail.com>
2017-12-16 01:13:38 +0530

    added indexing

--- cmd/blevedemo/main.go
+++ cmd/blevedemo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	bd "github.com/SKatiyar/blevedemo"
@@ -11,5 +12,10 @@ func main() {
 	mux.Handle("/", http.FileServer(http.Dir("../../public")))
 	mux.HandleFunc("/search", bd.Search)
 
+	if indexErr := bd.Index("../../public/sites.csv"); indexErr != nil {
+		panic(indexErr)
+	}
+
+	fmt.Println("Indexing completed, listening on :9090")
 	http.ListenAndServe(":9090", mux)
 }
--- indexer.go
+++ indexer.go
@@ -2,23 +2,59 @@ package blevedemo
 
 import (
 	"encoding/csv"
-	"fmt"
+	"io"
 	"os"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func Index() error {
-	file, fileError := os.Open("public/sites.csv")
+type Page struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Tags    string `json:"tags"`
+}
+
+func (p Page) Type() string {
+	return "page"
+}
+
+func Index(filePath string) error {
+	file, fileError := os.Open(filePath)
 	if fileError != nil {
 		return fileError
 	}
+	defer file.Close()
 
 	reader := csv.NewReader(file)
+	count := 0
 	for {
 		data, dataErr := reader.Read()
 		if dataErr != nil {
-			return dataErr
+			if dataErr != io.EOF {
+				return dataErr
+			}
+
+			return nil
+		}
+		if len(data) != 2 {
+			continue
 		}
 
-		fmt.Println(data)
+		doc, docErr := goquery.NewDocument("http://" + data[0])
+		if docErr != nil {
+			return docErr
+		}
+
+		if indexErr := indexer.Index(strconv.Itoa(count), Page{
+			Title:   doc.Find("title").Text(),
+			Content: doc.Find("body").Find("script").Remove().End().Text(),
+			Tags:    data[1],
+		}); indexErr != nil {
+			return indexErr
+		}
+
+		count += 1
 	}
+
 }
